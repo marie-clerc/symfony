88b3c908816ba7167f88d5e805c79f5cefd4c1ef
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-09-21 15:23:57 +0200

    controller, twig de base, fixtures, emmet

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for($i = 1; $i <= 10; $i++) {
+            $article = new Article();
+            $article->setTitle("Titre  de l'article n°$i")
+                    ->setContent("<p>Contenu de l'article $i</p>")
+                    ->setImage("http://placehold.it/350x150")
+                    ->setCreateAt(new \Datetime());
+            
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
